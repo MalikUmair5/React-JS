b05730511b1ebe91bdfa3e567fb41dbd95d3e972
“[Malik <[malikumair.dev@gmail.com]>
2024-07-11 13:18:53 -0700

    Understanding React Components

---
+++ understanding react components/src/App.tsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+
+function App(): JSX.Element {
+
+  return <>
+    <h1>Understanding Components</h1>
+    <Button></Button>
+  </>
+}
+
+export default App;
---
+++ understanding react components/src/Button.tsx
@@ -0,0 +1,10 @@
+
+function Button():JSX.Element {
+  return  <>
+    <button>Click me</button>
+    <button>Click me</button>
+  </>
+  
+}
+
+export default Button;
