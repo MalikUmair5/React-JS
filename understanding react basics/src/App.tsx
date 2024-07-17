848f615d139237d99b5827841b624e8ff122f253
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-17 15:44:03 +0500

    understanding passing children to components

--- understanding react basics/src/App.tsx
+++ understanding react basics/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Container from "./components/Container";
 import ErrorMessage from "./components/errorMessage";
 import FoodItems from "./components/foodItems";
 
@@ -6,14 +7,17 @@ function App() {
   // let healtyfoods = [];
   let healtyfoods = ["daal", "roti", "salad", "sabzi", "doodh", "fruits"];
 
- 
   return (
     <>
       <div className="main">
-        <h1>Healty Foods</h1>
-        <ErrorMessage foodItems = {healtyfoods}/>
-        <FoodItems foodItems = {healtyfoods}/>
-      
+        <Container>
+          <h1 className="mainheading">Healty Foods</h1>
+          <ErrorMessage foodItems={healtyfoods} />
+          <FoodItems foodItems={healtyfoods} />
+        </Container>
+        <Container>
+          <p>This is the list of healthy foods that we should eat</p>
+        </Container>
       </div>
     </>
   );
