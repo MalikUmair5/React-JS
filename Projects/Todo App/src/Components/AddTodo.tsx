bec28948147c24b3372aa13e6a6551ea1723ee84
[Malik Umair] <[malikumair.dev@gmail.com]>
2024-07-20 11:46:06 +0500

    Using form element in react

--- Projects/Todo App/src/Components/AddTodo.tsx
+++ Projects/Todo App/src/Components/AddTodo.tsx
@@ -1,55 +1,54 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import styles from "./AddTodo.module.css";
 import { FaPlusCircle } from "react-icons/fa";
 
-
-interface AddTodoProps{
-    handleAddTodo: (todo: string, date: string) => void;
+interface AddTodoProps {
+  handleAddTodo: (todo: string, date: string) => void;
 }
 
-function AddTodo({handleAddTodo}: AddTodoProps): JSX.Element {
-
+function AddTodo({ handleAddTodo }: AddTodoProps): JSX.Element {
   let [todoValue, setTodoValue] = useState("");
   let [todoDate, setTodoDate] = useState("");
 
-  const handleClick = () => {;
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
     handleAddTodo(todoValue, todoDate);
-
     setTodoValue("");
     setTodoDate("");
-  }
-
-    
-
+  };
 
   return (
     <div className="container addTodo">
-      <div className="row">
-        <div className="col-6">
-          <input
-            className={styles.todoInput}
-            type="text"
-            placeholder="Enter Todo here"
-            value={todoValue}
-            onChange={(e) => setTodoValue(e.target.value)}
-          />
+      <form action="" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-6">
+            <input
+              className={styles.todoInput}
+              type="text"
+              placeholder="Enter Todo here"
+              value={todoValue}
+              onChange={(e) => setTodoValue(e.target.value)}
+            />
+          </div>
+          <div className="col-4">
+            <input
+              className={styles.todoInput}
+              type="Date"
+              onChange={(e) => setTodoDate(e.target.value)}
+              value={todoDate}
+            />
+          </div>
+          <div className="col-2">
+            <button
+              type="submit"
+              className="btn btn-success"
+            >
+              <FaPlusCircle />
+            </button>
+          </div>
         </div>
-        <div className="col-4">
-          <input
-            className={styles.todoInput}
-            type="Date"
-            onChange={(e) => setTodoDate(e.target.value)}
-            value={todoDate}
-          />
-        </div>
-        <div className="col-2">
-          <button onClick={handleClick} type="button" className="btn btn-success">
-          <FaPlusCircle />
-
-          </button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
